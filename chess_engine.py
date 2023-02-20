dbeafc895e57e413142390f09e667b71ada95dbc
AlimulK <samskarim@gmail.com>
2023-02-20 20:48:47 +0000

    init board

--- chess_engine.py
+++ chess_engine.py
@@ -1,15 +1,20 @@
-"""
-Responsible for storing the current game's information, for determining
-valid moves for the current state and a history of the moves taken
+"""Responsible for storing the current game's information,
+for determining valid moves for the current state and a history of the moves taken
 """
 
 import numpy as np
 
 class GameState():
-    """
-    Class for the game state, initialises the board.
-    """
-    def __init__(self):
+    """Class for the game state, initialises the board."""
+
+    def __init__(self) -> None:
+        """Sets up the board
+
+        Sets up the board as an 8x8 Numpy 2d array. Each piece is represented by 2 characters,
+        the first character represents the colour, second character represents piece:
+        R = Rook, N = Knight, B = Bishop, Q = Queen and K = King. -- represents empty space.
+        """
+
         self.board = np.array([
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
