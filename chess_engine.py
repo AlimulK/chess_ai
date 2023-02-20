d2559a42d93c2ade39b557a4ccf58d2bf8239506
AlimulK <samskarim@gmail.com>
2023-02-20 20:26:41 +0000

    add numpy and basic setup

---
+++ chess_engine.py
@@ -0,0 +1,22 @@
+"""
+Responsible for storing the current game's information, for determining
+valid moves for the current state and a history of the moves taken
+"""
+
+import numpy as np
+
+class GameState():
+    """
+    Class for the game state, initialises the board.
+    """
+    def __init__(self):
+        self.board = np.array([
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ])
---
+++ chess_main.py
@@ -0,0 +1,4 @@
+"""
+Responsible for user input and displaying the current GameState,
+basically the user's interface with the game
+"""
