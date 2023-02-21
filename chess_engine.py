12ab240837cffe73a63e242f9824df042a45a143
AlimulK <samskarim@gmail.com>
2023-02-21 18:44:43 +0000

    start Move class

--- chess_engine.py
+++ chess_engine.py
@@ -8,7 +8,7 @@ import numpy as np
 class GameState():
     """Class for the game state."""
 
-    def __init__(self) -> None:
+    def __init__(self):
         """Sets up the board, whose move it is and a movelog
 
         Sets up the board as an 8x8 Numpy 2d array. Each piece is represented by 2 characters,
@@ -30,3 +30,19 @@ class GameState():
         self.white_to_move: bool = True
 
         self.movelog = []
+
+
+class Move():
+    """A class for moving the pieces"""
+
+    def __init__(self, start_sq: tuple[int, int], end_sq: tuple[int, int], board):
+        """Setup all the coordinate stuff"""
+
+        self.start_row = start_sq[0]
+        self.start_col = start_sq[1]
+        self.end_row = end_sq[0]
+        self.end_col = end_sq[1]
+        self.piece_moved = board[self.start_row][self.start_col]
+        self.piece_captured = board[self.end_row][self.end_col]
+
+    
